0ee1c17bd3370b4b5b931d998c8a7e9c23bebcc1
Takeshi Katano <takatano@outlook.com>
2020-07-15 02:23:31 +0900

    Add listvm

---
+++ hv-mmi-lab/listvm/HvVirtualMachine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Management.Infrastructure;
+
+namespace listvm
+{
+    internal class HvVirtualMachine
+    {
+        public Guid VMGuid { get; protected set; }
+        public string DisplayName { get; protected set; }
+        public DateTime InstallDate { get; protected set; }
+        public DateTime TimeOfLastConfigurationChange { get; protected set; }
+        public DateTime TimeOfLastStateChange { get; protected set; }
+
+        public HvVirtualMachine(CimInstance instance)
+        {
+            VMGuid = new Guid(instance.CimInstanceProperties["Name"].Value.ToString());
+            DisplayName = instance.CimInstanceProperties["ElementName"].Value.ToString();
+            InstallDate = (DateTime) instance.CimInstanceProperties["InstallDate"].Value;
+            TimeOfLastConfigurationChange = (DateTime) instance.CimInstanceProperties["TimeOfLastConfigurationChange"].Value;
+            TimeOfLastStateChange = (DateTime) instance.CimInstanceProperties["TimeOfLastStateChange"].Value;
+        }
+    }
+}
---
+++ hv-mmi-lab/listvm/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.Management.Infrastructure;
+using Microsoft.Management.Infrastructure.Options;
+
+namespace listvm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                PrintUsage();
+                return;
+            }
+
+            var hvHostMachine = args[0];
+            var vms = GetVMs(hvHostMachine);
+            PrintVMList(vms);
+        }
+
+        private const string VirtualizationNamespace = @"root\virtualization\v2";
+
+        private static IReadOnlyCollection<HvVirtualMachine> GetVMs(string hvHostMachine)
+        {
+            var vms = new List<HvVirtualMachine>();
+
+            var options = new CimSessionOptions();
+            using (var session = CimSession.Create(hvHostMachine, options))
+            {
+                const string query = "SELECT * FROM Msvm_ComputerSystem Where Caption = 'Virtual Machine'";
+                var cimOpsOptions = new CimOperationOptions();
+                var computerSystems = session.QueryInstances(VirtualizationNamespace, "WQL", query, cimOpsOptions);
+
+                foreach (var instance in computerSystems)
+                {
+                    vms.Add(new HvVirtualMachine(instance));
+                    instance.Dispose();
+                }
+            }
+
+            return vms.AsReadOnly();
+        }
+
+        private static void PrintVMList(IReadOnlyCollection<HvVirtualMachine> vms)
+        {
+            foreach (var vm in vms)
+            {
+                Console.WriteLine("DisplayName: " + vm.DisplayName);
+                Console.WriteLine("VMGuid: " + vm.VMGuid);
+                Console.WriteLine("InstallDate: " + vm.InstallDate);
+                Console.WriteLine("TimeOfLastConfigurationChange: " + vm.TimeOfLastConfigurationChange);
+                Console.WriteLine("TimeOfLastStateChange: " + vm.TimeOfLastStateChange);
+                Console.WriteLine();
+            }
+        }
+
+        private static void PrintUsage()
+        {
+            var exeName = Assembly.GetExecutingAssembly().GetName().Name;
+            Console.WriteLine(@"Usage: {0} hyperv_host", exeName);
+            Console.WriteLine();
+            Console.WriteLine("  Example: {0} localhost", exeName);
+            Console.WriteLine("  Example: {0} hvhost1", exeName);
+            Console.WriteLine("  Example: {0} hvhost1.internal.example.com", exeName);
+        }
+    }
+}
